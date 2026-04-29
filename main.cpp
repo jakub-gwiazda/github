f8c13ae9ec6140adad5f1424ca4991f2a2e5a60c
jakubkab <260515@edu.p.lodz.pl>
2026-04-29 16:17:50 +0200

    added divide

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ void usage(char *programName) {
     printf("\tadd\n");
     printf("\tpower\n");
     printf("\tmul\n");
+    printf("\tdivide\n");
     printf("\n");
 }
 
@@ -16,7 +17,7 @@ int main(int argc, char* argv[]) {
     int number1 = 0;
     int number2 = 0;
     int result = 0;
-// ee
+
     if (argc == 4) {
         number1 = atoi(argv[1]);
         number2 = atoi(argv[3]);
@@ -35,6 +36,15 @@ int main(int argc, char* argv[]) {
         if (!strcmp("mul", argv[2])) {
             result = number1 * number2;
             printf("%d * %d = %d\n", number1, number2, result);
+            return 0;
+        }
+        if (!strcmp("divide", argv[2])) {
+            if(number2 == 0){
+                printf("Can't divide by 0");
+            }else{
+                result = number1 / number2;
+                printf("%d / %d = %d\n", number1, number2, result);
+            }
             return 0;
         }
 
