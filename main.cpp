4d08afed586267677eb21796cc0c86fb7dcc1cd6
jakub-gwiazda <260501@edu.p.lodz.pl>
2026-04-29 16:24:44 +0200

    added power

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
     int number1 = 0;
     int number2 = 0;
     int result = 0;
-
+// ee
     if (argc == 4) {
         number1 = atoi(argv[1]);
         number2 = atoi(argv[3]);
